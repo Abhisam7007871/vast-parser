930c946afdc8230c44ef09e805b23a4b58323520
Abhishek <abhisam7007871@gmail.com>
2024-09-23 15:28:53 +0530

    Final Commit

--- src/main/java/com/avshek/vast_parser/controller/VastTagController.java
+++ src/main/java/com/avshek/vast_parser/controller/VastTagController.java
@@ -14,6 +14,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.FileNotFoundException;
+
 @RestController
 @RequestMapping("/vast")
 public class VastTagController {
@@ -22,7 +24,7 @@ public class VastTagController {
     private VastTagService vastTagService;
 
     @PostMapping("/parse/file")
-    public ResponseEntity<String> parseVastFile(@RequestParam String filePath) {
+    public ResponseEntity<String> parseVastFile(@RequestParam String filePath) throws FileNotFoundException {
         String xmlContent = vastTagService.readXmlFromFile(filePath);
         if (xmlContent == null) {
             return ResponseEntity.badRequest().body("Error reading XML file.");
@@ -77,48 +79,3 @@ public class VastTagController {
 
 
 
-
-
-//package com.avshek.vast_parser.controller;
-//
-//import com.avshek.vast_parser.model.VastTag;
-//import com.avshek.vast_parser.service.VastTagService;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.http.ResponseEntity;
-//import org.springframework.web.bind.annotation.*;
-//
-//@RestController
-//@RequestMapping("/vast")
-//public class VastTagController {
-//
-//    @Autowired
-//    private VastTagService vastTagService;
-//
-//    @PostMapping("/parse/file")
-//    public ResponseEntity<String> parseVastFile(@RequestParam String filePath) {
-//        String xmlContent = vastTagService.readXmlFromFile(filePath);
-//        if (xmlContent == null) {
-//            return ResponseEntity.badRequest().body("Error reading XML file.");
-//        }
-//        VastTag vastTag = vastTagService.parseVastXml(xmlContent);
-//        vastTagService.saveVastTag(vastTag);
-//        return ResponseEntity.ok("VAST file parsed and saved.");
-//    }
-//
-//    @PostMapping("/parse/url")
-//    public ResponseEntity<String> parseVastUrl(@RequestParam String url) {
-//        String xmlContent = vastTagService.readXmlFromUrl(url);
-//        if (xmlContent == null) {
-//            return ResponseEntity.badRequest().body("Error reading XML from URL.");
-//        }
-//        VastTag vastTag = vastTagService.parseVastXml(xmlContent);
-//        vastTagService.saveVastTag(vastTag);
-//        return ResponseEntity.ok("VAST URL parsed and saved.");
-//    }
-//
-//    @GetMapping("/{id}")
-//    public ResponseEntity<VastTag> getVastTagById(@PathVariable Long id) {
-//        VastTag vastTag = vastTagService.findById(id);
-//        return vastTag != null ? ResponseEntity.ok(vastTag) : ResponseEntity.notFound().build();
-//    }
-//}
