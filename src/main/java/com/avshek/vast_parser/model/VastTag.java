7614a384f618ac1d9a47b3837b565714187b1ea5
Abhishek Sharma <102051093+Abhisam7007871@users.noreply.github.com>
2024-10-08 18:39:52 +0530

    Update VastData.java

--- src/main/java/com/avshek/vast_parser/model/VastTag.java
+++ src/main/java/com/avshek/vast_parser/model/VastTag.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 @Entity
 @Table(name = "vast_tags")
-public class VastTag {
+public class Vastg {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
